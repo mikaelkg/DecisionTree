44108a8353109d4c450463acbda3ff804cc8065f
mikaelkg <5019973@mail.ru>
2018-06-06 01:21:18 +0300

    stage4

--- _build/CMakeFiles/dectree.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for dectree.
-# This may be replaced when dependencies are built.
--- _build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikael/mikaelkg/workspace/projects/DecisionTree
-# Build directory: /home/mikael/mikaelkg/workspace/projects/DecisionTree/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/mikael/mikaelkg/workspace/projects/DecisionTree/_build/check" "-s" "-r" "compact" "--use-colour" "yes")
