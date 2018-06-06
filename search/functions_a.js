a23f7932ba8cdf3f547b74edcbb97770e572ea4a
mikaelkg <5019973@mail.ru>
2018-06-06 18:09:19 +0300

    documentation

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['pre_5forder',['pre_order',['../classDecisionTree_1_1DecTree.html#a9b606f67b50ab18e2b85081e3ea5add7',1,'DecisionTree::DecTree']]],
-  ['predict',['predict',['../classDecisionTree_1_1DecTree.html#a942e0cbf7c51688cb48b812e5c21736f',1,'DecisionTree::DecTree']]]
+  ['predict',['predict',['../classDecisionTree_1_1DecTree.html#a942e0cbf7c51688cb48b812e5c21736f',1,'DecisionTree::DecTree']]],
+  ['print',['print',['../classTUI.html#a2d3fc1e9806dbc4b418d4223f8d81030',1,'TUI::print(std::vector&lt; std::string &gt; &amp;real_class, std::vector&lt; std::string &gt; &amp;predicted_class, double accuracy)'],['../classTUI.html#a4acf4e7bc9d914849e0b3d3dce6b1b22',1,'TUI::print(std::vector&lt; std::vector&lt; double &gt;&gt; &amp;features_to_predict, std::vector&lt; std::string &gt; &amp;predicted_class)']]]
 ];
